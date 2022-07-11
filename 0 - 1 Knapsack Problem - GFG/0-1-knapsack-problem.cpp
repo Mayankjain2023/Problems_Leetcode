367e0dd9d2c575802e6257ae860904e599d242d1
Mayank Jain <85195380+Mayankjain2023@users.noreply.github.com>
2022-07-11 22:16:46 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -14,59 +14,59 @@ class Solution
        // Your code here
        
        
-       int dp[n+1][W+1];
-       //initialisation of dp
-       for(int i=0;i<n+1;i++){
-           for(int j=0;j<W+1;j++){
-               if(i==0 || j==0){
-                   dp[i][j]=0;
-               }
-           }
-       }
-       //populating dp
-       for(int i=1;i<n+1;i++){
-           for(int j=1;j<W+1;j++){
-               if(wt[i-1]<=j){
-                   dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
-               }
-               else{
-                   dp[i][j]=dp[i-1][j];
-               }
-           }
-       }
-       return dp[n][W];
+    //   int dp[n+1][W+1];
+    //   //initialisation of dp
+    //   for(int i=0;i<n+1;i++){
+    //       for(int j=0;j<W+1;j++){
+    //           if(i==0 || j==0){
+    //               dp[i][j]=0;
+    //           }
+    //       }
+    //   }
+    //   //populating dp
+    //   for(int i=1;i<n+1;i++){
+    //       for(int j=1;j<W+1;j++){
+    //           if(wt[i-1]<=j){
+    //               dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+    //           }
+    //           else{
+    //               dp[i][j]=dp[i-1][j];
+    //           }
+    //       }
+    //   }
+    //   return dp[n][W];
     
        
        
        
        
-    //   int t[n+1][W+1];
-    //   for(int i=0;i<n+1;i++)
-    //   {
-    //       if(i==0) t[i][0]=0;
-    //   }
+      int t[n+1][W+1];
+      for(int i=0;i<n+1;i++)
+      {
+                for(int j=0;j<W+1;j++)
+                {
+                    if(i==0||j==0) {
+                        t[i][j]=0;
+                    }
+                }
+      }
        
-    //   for(int j=0;j<W+1;j++)
-    //   {
-    //       if(j==0) t[0][j]=0;
-    //   }
-       
-    //   for(int i=1;i<n+1;i++)
-    //   {
-    //       for(int j=1;j<W+1;j++)
-    //       {
+      for(int i=1;i<n+1;i++)
+      {
+          for(int j=1;j<W+1;j++)
+          {
               
-    //           if(wt[i-1]<=j)
-    //           {
-    //               t[i][j]=max(t[i-1][j],val[i-1]+t[i-1][j-wt[i-1]]);
-    //           }
-    //           else
-    //           {
-    //               t[i][j]=t[i-1][j];
-    //           }
-    //       }
-    //   }
-    //   return t[n][W];
+              if(wt[i-1]<=j)
+              {
+                  t[i][j]=max(t[i-1][j],val[i-1]+t[i-1][j-wt[i-1]]);
+              }
+              else
+              {
+                  t[i][j]=t[i-1][j];
+              }
+          }
+      }
+      return t[n][W];
        
     }
 };
