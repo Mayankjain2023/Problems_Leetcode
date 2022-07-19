1289b0b02ff9f94d7247a74ce0c45ce703fab080
Mayank Jain <85195380+Mayankjain2023@users.noreply.github.com>
2022-07-19 22:56:08 +0530

    Time: 4 ms (25.27%), Space: 6.5 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -22,22 +22,22 @@ public:
             list.push_back({1,1});
              int level=2;
                 
-            for(int i=1;i<n-1;i++)
+            for(int i=2;i<=n-1;i++)
             {   
                 vector<int>res;
                 for(int j=0;j<=level;j++)
                 {
                     if(j==0)
                     {
-                        res.push_back(list[i][0]);
+                        res.push_back(list[i-1][0]);
                     }
                     else if(j==level)
                     {
-                        res.push_back(list[i][level-1]);
+                        res.push_back(list[i-1][level-1]);
                     }
                     else
                     {
-                        res.push_back(list[i][j-1]+list[i][j]);
+                        res.push_back(list[i-1][j-1]+list[i-1][j]);
                     }
                 }
                 level++;
