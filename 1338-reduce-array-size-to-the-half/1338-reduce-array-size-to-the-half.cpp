0711be2bef2ed41e4d980f3f5ce944087440a28b
Mayank Jain <85195380+Mayankjain2023@users.noreply.github.com>
2022-08-18 20:21:53 +0530

    Time: 287 ms (69.75%), Space: 78.2 MB (79.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
+        int ans=0;
+        int set_size=0;
         
+        unordered_map<int,int>freq_set;
         
-        int n=arr.size()/2;
-        
-        
-        sort(arr.begin(),arr.end());
-        
-        unordered_map<int,int>map;
-        for(int i=0;i<arr.size();i++)
-        {
-            map[arr[i]]++;
+        int size=arr.size();
+        for(int i=0;i<size;i++){
+            freq_set[arr[i]]++;
         }
         
-       
-        vector<int>vec;
-        
-        for(auto a:map)
-        {
-            vec.push_back(a.second);
+        vector<int>v;
+        for(auto x:freq_set){
+            v.push_back(x.second);
         }
+        sort(v.begin(),v.end());
         
-        sort(vec.begin(),vec.end());
-        int count=0;
-        int sum=0;
-        while(sum<n)
-        {
-            sum+=vec.back();
-            vec.pop_back();
-            count++;
+        if(v.size()==1) return 1;
+        
+        for(int i=v.size()-1;i>=0;i--){
+            if(ans>=size/2){
+                return set_size;
+            }
+            else{
+                set_size++;
+                ans=ans+v[i];
+            }
         }
-        
-        return count;
+        return set_size;
     }
 };
